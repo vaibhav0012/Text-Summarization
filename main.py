f72f59f60d2d24083ac74d504e16e7d43d0067bb
vaibhav0012 <vaibhavmehrao12@gmail.com>
2024-08-01 17:20:07 +0530

    Added utils function

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.Text_Summarization.logging import logger
+
+logger.info("Testing Logging")
---
+++ src/Text_Summarization/utils/common.py
@@ -0,0 +1,58 @@
+import sys
+import os
+from src.Text_Summarization.logging import logger
+from src.Text_Summarization.exception import CustomException
+import yaml
+from box.exceptions import BoxValueError
+from pathlib import Path
+from typing import Any
+from box import ConfigBox
+from ensure import ensure_annotations
+
+@ensure_annotations
+def read_yaml(path_to_yaml:Path) -> ConfigBox:
+    '''
+    Read Yaml files
+    Args: Path to yaml
+    Returns Config Box
+    '''
+
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise CustomException(e,sys)
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
+
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
+
+
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """get size in KB
+
+    Args:
+        path (Path): path of the file
+
+    Returns:
+        str: size in KB
+    """
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~ {size_in_kb} KB"
+
+    
